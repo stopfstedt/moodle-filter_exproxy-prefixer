5b03825b06c382d6ee09b274bcf9b473c34afd17
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-06-27 16:11:17 -0700

    configurable prefixer url.

--- filter.php
+++ filter.php
@@ -27,6 +27,7 @@ class filter_ucsfezproxy extends moodle_text_filter {
 
     /**
      * @inheritdoc
+     * @throws dml_exception
      */
     function filter($text, array $options = array()) {
 
@@ -35,11 +36,13 @@ class filter_ucsfezproxy extends moodle_text_filter {
             return $text;
         }
 
+        $prefix_url = trim(get_config('filter_ucsfezproxy', 'prefixurl')) ?: self::DEFAULT_PROXY_PREFIX;
+
         // find and prefix all URLs in the given text.
-        $text = preg_replace(self::URL_REGEX, self::DEFAULT_PROXY_PREFIX . '${0}', $text);
+        $text = preg_replace(self::URL_REGEX, $prefix_url . '${0}', $text);
 
         // eliminate double-prefixed URLs.
-        $text = str_ireplace(self::DEFAULT_PROXY_PREFIX . self::DEFAULT_PROXY_PREFIX, self::DEFAULT_PROXY_PREFIX, $text);
+        $text = str_ireplace($prefix_url . $prefix_url, $prefix_url, $text);
 
         return $text;
     }
--- lang/en/filter_ucsfezproxy.php
+++ lang/en/filter_ucsfezproxy.php
@@ -6,4 +6,6 @@
  * @package   filter_ucsfezproxy
  */
 $string['filtername'] = 'UCSF EZPRoxy URL Prefixer';
+$string['prefixurl'] = 'EZProxy Prefix URL';
+$string['prefixurl_help'] = 'The EZProxy URL used to prefix URLs in the filtered text contents.';
 $string['privacy:metadata'] = 'The UCSF EZProxy URL Prefixer plugin does not store any personal data.';
