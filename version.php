dd780a7d3299baccd4cd6ab4db69266c5de22dc6
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-07-08 13:42:59 -0700

    version bump.

--- version.php
+++ version.php
@@ -6,6 +6,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019070500;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2019070800;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2018112800;        // Requires this Moodle version
 $plugin->component = 'filter_ucsfezproxy';     // Full name of the plugin (used for diagnostics)
