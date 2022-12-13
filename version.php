d59e419cff03a353cb3977d6f08fee285328678f
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-12-12 16:25:45 -0800

    version and requirement bump to Moodle 4.1

--- version.php
+++ version.php
@@ -6,6 +6,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022110100;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2022041200;        // Requires this Moodle version
+$plugin->version   = 2022121200;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2022111800;        // Requires this Moodle version
 $plugin->component = 'filter_ucsfezproxy';     // Full name of the plugin (used for diagnostics)
